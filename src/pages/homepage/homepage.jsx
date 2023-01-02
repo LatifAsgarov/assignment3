7aa2485db2d783806ff55b8366629b1d02d518a2
JigmeT123 <tashinamgyaljigme@gmail.com>
2023-01-02 19:58:03 +0400

    feat(): button struct

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,9 +1,11 @@
+import Button from '../../components/button/button';
 import styles from './homepage.module.css';
 
 const Homepage = () => {
     return(
         <div className={styles.homepageContainer}>
             Homepage
+            <Button link="/contact-us" name="Contact Us" />
         </div>
     )
 }
